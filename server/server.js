4718fbdb922e74663e68e383c599e944c707f577
Mohd Aman Naim Patel <ap5277478@gmail.com>
2024-09-29 20:14:25 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-
 import express from "express";
 import { config } from "dotenv";
-config();
 import userRoutes from "./routes/game.routes.js";
+import path from "path";
+config();
+const __dirname = path.resolve();
+console.log("__dirname",__dirname)
 const app = express();
 app.use(express.json());
 
@@ -11,3 +13,8 @@ app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
 app.use("/api/game", userRoutes);
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
